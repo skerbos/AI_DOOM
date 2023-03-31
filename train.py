c25341e8d1caee15ec5ed2e7363e0bf2da2ca3c8
Tej-Deep <ptejdeep@gmail.com>
2023-03-31 13:57:07 +0800

    Edited files to seprate agents class from training code and added evaluation code

---
+++ train.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python3
+
+# E. Culurciello, L. Mueller, Z. Boztoprak
+# December 2020
+
+import itertools as it
+import os
+import random
+from collections import deque
+from time import sleep, time
+
+import numpy as np
+import skimage.transform
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import vizdoom as vzd
+from tqdm import trange
+
+from Agents.DQN import DQNAgent
+
+
+# Q-learning settings
+learning_rate = 0.00025
+discount_factor = 0.99
+train_epochs = 5
+learning_steps_per_epoch = 2000
+replay_memory_size = 10000
+
+# NN learning settings
+batch_size = 64
+
+# Training regime
+test_episodes_per_epoch = 100
+
+# Other parameters
+frame_repeat = 12
+resolution = (30, 45)
+episodes_to_watch = 10
+
+model_savefile = "./model-doom.pth"
+save_model = True
+load_model = False
+skip_learning = False
+
+# Configuration file path
+config_file_path = os.path.join(vzd.scenarios_path, "simpler_basic.cfg")
+# config_file_path = os.path.join(vzd.scenarios_path, "rocket_basic.cfg")
+# config_file_path = os.path.join(vzd.scenarios_path, "basic.cfg")
+
+# Uses GPU if available
+if torch.cuda.is_available():
+    DEVICE = torch.device("cuda")
+    torch.backends.cudnn.benchmark = True
+else:
+    DEVICE = torch.device("cpu")
+
+
+def preprocess(img):
+    """Down samples image to resolution"""
+    img = skimage.transform.resize(img, resolution)
+    img = img.astype(np.float32)
+    img = np.expand_dims(img, axis=0)
+    return img
+
+
+def create_simple_game():
+    print("Initializing doom...")
+    game = vzd.DoomGame()
+    game.load_config(config_file_path)
+    game.set_window_visible(False)
+    game.set_mode(vzd.Mode.PLAYER)
+    game.set_screen_format(vzd.ScreenFormat.GRAY8)
+    game.set_screen_resolution(vzd.ScreenResolution.RES_640X480)
+    game.init()
+    print("Doom initialized.")
+
+    return game
+
+
+def test(game, agent):
+    """Runs a test_episodes_per_epoch episodes and prints the result"""
+    print("\nTesting...")
+    test_scores = []
+    for test_episode in trange(test_episodes_per_epoch, leave=False):
+        game.new_episode()
+        while not game.is_episode_finished():
+            state = preprocess(game.get_state().screen_buffer)
+            best_action_index = agent.get_action(state)
+
+            game.make_action(actions[best_action_index], frame_repeat)
+        r = game.get_total_reward()
+        test_scores.append(r)
+
+    test_scores = np.array(test_scores)
+    print(
+        "Results: mean: {:.1f} +/- {:.1f},".format(
+            test_scores.mean(), test_scores.std()
+        ),
+        "min: %.1f" % test_scores.min(),
+        "max: %.1f" % test_scores.max(),
+    )
+
+
+def run(game, agent, actions, num_epochs, frame_repeat, steps_per_epoch=2000):
+    """
+    Run num epochs of training episodes.
+    Skip frame_repeat number of frames after each action.
+    """
+
+    start_time = time()
+    # print(dir(game))
+    # print(game.get_game_variable(vzd.GameVariable.KILLCOUNT))
+
+    for epoch in range(num_epochs):
+        game.new_episode()
+        train_scores = []
+        global_step = 0
+        print("\nEpoch #" + str(epoch + 1))
+
+        for _ in trange(steps_per_epoch, leave=False):
+            state = preprocess(game.get_state().screen_buffer)
+            action = agent.get_action(state)
+            reward = game.make_action(actions[action], frame_repeat) + game.get_game_variable(vzd.GameVariable.KILLCOUNT)
+            done = game.is_episode_finished()
+
+            if not done:
+                next_state = preprocess(game.get_state().screen_buffer)
+            else:
+                next_state = np.zeros((1, 30, 45)).astype(np.float32)
+
+            agent.append_memory(state, action, reward, next_state, done)
+
+            if global_step > agent.batch_size:
+                agent.train()
+
+            if done:
+                train_scores.append(game.get_total_reward())
+                game.new_episode()
+
+            global_step += 1
+
+        agent.update_target_net()
+        train_scores = np.array(train_scores)
+
+        print(
+            "Results: mean: {:.1f} +/- {:.1f},".format(
+                train_scores.mean(), train_scores.std()
+            ),
+            "min: %.1f," % train_scores.min(),
+            "max: %.1f," % train_scores.max(),
+        )
+
+        test(game, agent)
+        if save_model:
+            print("Saving the network weights to:", model_savefile)
+            torch.save(agent.q_net, model_savefile)
+        print("Total elapsed time: %.2f minutes" % ((time() - start_time) / 60.0))
+
+    game.close()
+    return agent, game
+
+if __name__ == "__main__":
+    # Initialize game and actions
+    game = create_simple_game()
+    n = game.get_available_buttons_size()
+    actions = [list(a) for a in it.product([0, 1], repeat=n)]
+
+    # Initialize our agent with the set parameters
+    agent = DQNAgent(
+        len(actions),
+        lr=learning_rate,
+        batch_size=batch_size,
+        memory_size=replay_memory_size,
+        discount_factor=discount_factor,
+        load_model=load_model,
+    )
+
+    # Run the training for the set number of epochs
+    if not skip_learning:
+        agent, game = run(
+            game,
+            agent,
+            actions,
+            num_epochs=train_epochs,
+            frame_repeat=frame_repeat,
+            steps_per_epoch=learning_steps_per_epoch,
+        )
+
+        print("======================================")
+        print("Training finished. It's time to watch!")
+
+    # Reinitialize the game with window visible
+    game.close()
+    game.set_window_visible(True)
+    game.set_mode(vzd.Mode.ASYNC_PLAYER)
+    game.init()
+
+    for _ in range(episodes_to_watch):
+        game.new_episode()
+        while not game.is_episode_finished():
+            state = preprocess(game.get_state().screen_buffer)
+            best_action_index = agent.get_action(state)
+
+            # Instead of make_action(a, frame_repeat) in order to make the animation smooth
+            game.set_action(actions[best_action_index])
+            for _ in range(frame_repeat):
+                game.advance_action()
+
+        # Sleep between episodes
+        sleep(1.0)
+        score = game.get_total_reward()
+        print("Total score: ", score)
