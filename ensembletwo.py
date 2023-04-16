3f7783181d0d645e9aaa4ce3dff4d77d4c85ecbc
sahana477 <84558364+sahana477@users.noreply.github.com>
2023-04-17 00:08:20 +0800

    AlexNet + ResNet

---
+++ ensembletwo.py
@@ -0,0 +1,374 @@
+#!/usr/bin/env python3
+
+# E. Culurciello, L. Mueller, Z. Boztoprak
+# December 2020
+
+import itertools as it
+import os
+import random
+from collections import deque
+from time import sleep, time
+
+import numpy as np
+import skimage.transform
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import vizdoom as vzd
+from tqdm import trange
+#from torchvision.models import alexnet, AlexNet_Weights
+import torchvision
+from torchinfo import summary
+
+
+# Q-learning settings
+learning_rate = 0.00025
+discount_factor = 0.99
+train_epochs = 5
+learning_steps_per_epoch = 2000
+replay_memory_size = 10000
+
+# NN learning settings
+batch_size = 64
+
+# Training regime
+test_episodes_per_epoch = 100
+
+# Other parameters
+frame_repeat = 12
+resolution = (240, 320)
+episodes_to_watch = 10
+
+model_savefile = "./model-doom.pth"
+save_model = True
+load_model = False
+skip_learning = False
+
+# Configuration file path
+config_file_path = os.path.join(vzd.scenarios_path, "defend_the_center.cfg")
+
+# Uses GPU if available
+if torch.cuda.is_available():
+    DEVICE = torch.device("cuda")
+    torch.backends.cudnn.benchmark = True
+    print("running on my GPU")
+else:
+    DEVICE = torch.device("cpu")
+    print("sad life")
+
+def preprocess(img):
+    """Down samples image to resolution"""
+    img = skimage.transform.resize(img, resolution)
+    img = img.astype(np.float32)
+    img = np.expand_dims(img, axis=0)
+    return img
+
+
+def create_simple_game():
+    print("Initializing doom...")
+    game = vzd.DoomGame()
+    game.load_config(config_file_path)
+    game.set_window_visible(False)
+    game.set_mode(vzd.Mode.PLAYER)
+    game.set_screen_format(vzd.ScreenFormat.GRAY8)
+    game.set_screen_resolution(vzd.ScreenResolution.RES_640X480)
+    game.set_doom_scenario_path(os.path.join(vzd.scenarios_path, "DOOM.WAD"))
+    game.set_doom_map("E1M1")
+    game.init()
+    print("Doom initialized.")
+
+    return game
+
+
+def test(game, agent):
+    """Runs a test_episodes_per_epoch episodes and prints the result"""
+    print("\nTesting...")
+    test_scores = []
+    for test_episode in trange(test_episodes_per_epoch, leave=False):
+        game.new_episode()
+        while not game.is_episode_finished():
+            state = preprocess(game.get_state().screen_buffer)
+            best_action_index = agent.get_action(state)
+
+            game.make_action(actions[best_action_index], frame_repeat)
+        r = game.get_total_reward()
+        test_scores.append(r)
+
+    test_scores = np.array(test_scores)
+    print(
+        "Results: mean: {:.1f} +/- {:.1f},".format(
+            test_scores.mean(), test_scores.std()
+        ),
+        "min: %.1f" % test_scores.min(),
+        "max: %.1f" % test_scores.max(),
+    )
+
+
+def run(game, agent, actions, num_epochs, frame_repeat, steps_per_epoch=2000):
+    """
+    Run num epochs of training episodes.
+    Skip frame_repeat number of frames after each action.
+    """
+
+    start_time = time()
+    # print(dir(game))
+    # print(game.get_game_variable(vzd.GameVariable.KILLCOUNT))
+
+    for epoch in range(num_epochs):
+        game.new_episode()
+        train_scores = []
+        global_step = 0
+        print("\nEpoch #" + str(epoch + 1))
+
+        for _ in trange(steps_per_epoch, leave=False):
+            state = preprocess(game.get_state().screen_buffer)
+            action = agent.get_action(state)
+            reward = game.make_action(actions[action], frame_repeat) + game.get_game_variable(vzd.GameVariable.KILLCOUNT)
+            done = game.is_episode_finished()
+
+            if not done:
+                next_state = preprocess(game.get_state().screen_buffer)
+            else:
+                next_state = np.zeros((1, resolution[0], resolution[1])).astype(np.float32)
+                # next_state  = 
+
+            agent.append_memory(state, action, reward, next_state, done)
+
+            if global_step > agent.batch_size:
+                agent.train()
+
+            if done:
+                train_scores.append(game.get_total_reward())
+                game.new_episode()
+
+            global_step += 1
+
+        agent.update_target_net()
+        train_scores = np.array(train_scores)
+
+        print(
+            "Results: mean: {:.1f} +/- {:.1f},".format(
+                train_scores.mean(), train_scores.std()
+            ),
+            "min: %.1f," % train_scores.min(),
+            "max: %.1f," % train_scores.max(),
+        )
+
+        test(game, agent)
+        if save_model:
+            print("Saving the network weights to:", model_savefile)
+            torch.save(agent.q_net, model_savefile)
+        print("Total elapsed time: %.2f minutes" % ((time() - start_time) / 60.0))
+    
+    game.close()
+    return agent, game
+
+
+
+class ensemblemodel(nn.Module):
+    def __init__(self,available_actions_count) -> None:
+        super().__init__()
+        self.state_fc = nn.Sequential(nn.Linear(64, 32), nn.ReLU(), nn.Linear(32, 1))
+        self.advantage_fc = nn.Sequential(
+            nn.Linear(64, 32), nn.ReLU(), nn.Linear(32, available_actions_count)
+        )
+        #self.state_fc = nn.Sequential(nn.Linear(64, 32), nn.ReLU(), nn.Linear(64, 1))
+
+        self.initial_layer = nn.Sequential(nn.Conv2d(1,3,3,1,1), nn.ReLU())
+        self.initial_layerb = nn.Sequential(nn.Conv2d(1,3,3,1,1), nn.ReLU())
+        
+        resnet = torchvision.models.resnet50(weights = torchvision.models.ResNet50_Weights.DEFAULT)
+        alexnet = torchvision.models.alexnet(weights = torchvision.models.AlexNet_Weights.DEFAULT)
+        # Telling the model to keep pre-trained weights
+        for param in resnet.parameters():    
+            param.requires_grad = False
+        for param in alexnet.parameters():
+            param.requires_grad = False
+        self.model_1 = resnet
+        self.model_2 = alexnet
+        
+        self.model_1 = nn.Sequential(
+            self.initial_layer,
+            self.model_1,
+            nn.ReLU(),
+            nn.Linear(in_features=1000, out_features=512),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=256),
+            nn.ReLU(),
+            nn.Linear(in_features=256, out_features=128),
+            nn.ReLU(),
+            
+            )
+        self.model_2 = nn.Sequential(
+            self.initial_layerb,
+            self.model_2,
+            nn.ReLU(),
+            nn.Linear(in_features=1000, out_features=512),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=256),
+            nn.ReLU(),
+            nn.Linear(in_features=256, out_features=128),
+            nn.ReLU(),
+            
+            )
+       
+    def forward(self, x):
+        
+        x_1 = self.model_1(x)
+        x_2= self.model_2(x)
+        x = x_1+ x_2
+        
+        x1 = x[:, :64]  # input for the net to calculate the state value
+        x2 = x[:, 64:]  # relative advantage of actions in the state
+        state_value = self.state_fc(x1).reshape(-1, 1)
+        advantage_values = self.advantage_fc(x2)
+        x = state_value + (
+            advantage_values - advantage_values.mean(dim=1).reshape(-1, 1)
+        )
+        return x
+
+
+class DQNAgent:
+    def __init__(
+        self,
+        action_size,
+        memory_size,
+        batch_size,
+        discount_factor,
+        lr,
+        load_model,
+        epsilon=1,
+        epsilon_decay=0.9996,
+        epsilon_min=0.1,
+    ):
+        self.action_size = action_size
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.epsilon_min = epsilon_min
+        self.batch_size = batch_size
+        self.discount = discount_factor
+        self.lr = lr
+        self.memory = deque(maxlen=memory_size)
+        self.criterion = nn.MSELoss()
+
+        if load_model:
+            print("Loading model from: ", model_savefile)
+            self.q_net = torch.load(model_savefile)
+            self.target_net = torch.load(model_savefile)
+            self.epsilon = self.epsilon_min
+
+        else:
+            print("Initializing new model")
+            self.q_net = ensemblemodel(action_size).to(DEVICE)
+            self.target_net = ensemblemodel(action_size).to(DEVICE)
+
+        self.opt = optim.SGD(self.q_net.parameters(), lr=self.lr)
+
+    def get_action(self, state):
+        if np.random.uniform() < self.epsilon:
+            return random.choice(range(self.action_size))
+        else:
+            state = np.expand_dims(state, axis=0)
+            state = torch.from_numpy(state).float().to(DEVICE)
+            action = torch.argmax(self.q_net(state)).item()
+            return action
+
+    def update_target_net(self):
+        self.target_net.load_state_dict(self.q_net.state_dict())
+
+    def append_memory(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def train(self):
+        batch = random.sample(self.memory, self.batch_size)
+        batch = np.array(batch, dtype=object)
+
+        states = np.stack(batch[:, 0]).astype(float)
+        actions = batch[:, 1].astype(int)
+        rewards = batch[:, 2].astype(float)
+        next_states = np.stack(batch[:, 3]).astype(float)
+        dones = batch[:, 4].astype(bool)
+        not_dones = ~dones
+
+        row_idx = np.arange(self.batch_size)  # used for indexing the batch
+
+        # value of the next states with double q learning
+        # see https://arxiv.org/abs/1509.06461 for more information on double q learning
+        with torch.no_grad():
+            next_states = torch.from_numpy(next_states).float().to(DEVICE)
+            idx = row_idx, np.argmax(self.q_net(next_states).cpu().data.numpy(), 1)
+            next_state_values = self.target_net(next_states).cpu().data.numpy()[idx]
+            next_state_values = next_state_values[not_dones]
+
+        # this defines y = r + discount * max_a q(s', a)
+        q_targets = rewards.copy()
+        q_targets[not_dones] += self.discount * next_state_values
+        q_targets = torch.from_numpy(q_targets).float().to(DEVICE)
+
+        # this selects only the q values of the actions taken
+        idx = row_idx, actions
+        states = torch.from_numpy(states).float().to(DEVICE)
+        action_values = self.q_net(states)[idx].float().to(DEVICE)
+
+        self.opt.zero_grad()
+        td_error = self.criterion(q_targets, action_values)
+        td_error.backward()
+        self.opt.step()
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+        else:
+            self.epsilon = self.epsilon_min
+
+
+if __name__ == "__main__":
+    # Initialize game and actions
+    game = create_simple_game()
+    n = game.get_available_buttons_size()
+    actions = [list(a) for a in it.product([0, 1], repeat=n)]
+
+    # Initialize our agent with the set parameters
+    agent = DQNAgent(
+        len(actions),
+        lr=learning_rate,
+        batch_size=batch_size,
+        memory_size=replay_memory_size,
+        discount_factor=discount_factor,
+        load_model=load_model,
+    )
+    #summary(alexnetmodel, input_data=(1,1,480,640))
+    # Run the training for the set number of epochs
+    if not skip_learning:
+        agent, game = run(
+            game,
+            agent,
+            actions,
+            num_epochs=train_epochs,
+            frame_repeat=frame_repeat,
+            steps_per_epoch=learning_steps_per_epoch,
+        )
+
+        print("======================================")
+        print("Training finished. It's time to watch!")
+
+    # Reinitialize the game with window visible
+    game.close()
+    game.set_window_visible(True)
+    game.set_mode(vzd.Mode.ASYNC_PLAYER)
+    game.init()
+
+    for _ in range(episodes_to_watch):
+        game.new_episode()
+        while not game.is_episode_finished():
+            state = preprocess(game.get_state().screen_buffer)
+            best_action_index = agent.get_action(state)
+
+            # Instead of make_action(a, frame_repeat) in order to make the animation smooth
+            game.set_action(actions[best_action_index])
+            for _ in range(frame_repeat):
+                game.advance_action()
+
+        # Sleep between episodes
+        sleep(1.0)
+        score = game.get_total_reward()
+        print("Total score: ", score)
